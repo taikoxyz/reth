4e416e218a4454881a9792fd56bbab8fed98c3f8
smtmfft <smtm@taiko.xyz>
2024-11-23 09:50:44 +0000

    use k256 to recovery sign for risc0

--- crates/primitives/src/transaction/util.rs
+++ crates/primitives/src/transaction/util.rs
@@ -15,11 +15,8 @@ pub(crate) mod secp256k1 {
     /// This does not ensure that the `s` value in the signature is low, and _just_ wraps the
     /// underlying secp256k1 library.
     pub fn recover_signer_unchecked(sig: &[u8; 65], msg: &[u8; 32]) -> Result<Address, Error> {
-        let sig =
-            RecoverableSignature::from_compact(&sig[0..64], RecoveryId::from_i32(sig[64] as i32)?)?;
-
-        let public = SECP256K1.recover_ecdsa(&Message::from_digest(*msg), &sig)?;
-        Ok(public_key_to_address(public))
+        let address_bytes = k256::recover_signer_unchecked(sig, msg).map_err(|_| Error::InvalidSignature)?;
+        Ok(Address::from_slice(&address_bytes))
     }
 
     /// Signs message with the given secret key.
@@ -59,5 +56,61 @@ mod tests {
         let out = address!("c08b5542d177ac6686946920409741463a15dddb");
 
         assert_eq!(secp256k1::recover_signer_unchecked(&sig, &hash), Ok(out));
+    }
+}
+
+pub(crate) mod k256 {
+    use k256::ecdsa::signature::digest::Digest;
+    use k256::ecdsa::Error as K256_Error;
+    use k256::ecdsa::{RecoveryId, Signature, VerifyingKey};
+
+    use sha3::Keccak256;
+
+    pub fn recover_signer_unchecked(
+        sig: &[u8; 65],
+        msg: &[u8; 32],
+    ) -> Result<[u8; 20], K256_Error> {
+        let mut signature = Signature::from_slice(&sig[..64])?;
+        let mut recid_byte = sig[64];
+
+        if let Some(sig_normalized) = signature.normalize_s() {
+            signature = sig_normalized;
+            recid_byte ^= 1;
+        }
+        let recid = RecoveryId::try_from(recid_byte).map_err(|_| K256_Error::default())?;
+
+        let pk = VerifyingKey::recover_from_prehash(msg, &signature, recid)?;
+
+        Ok(public_key_to_address(&pk))
+    }
+
+    fn public_key_to_address(public_key: &VerifyingKey) -> [u8; 20] {
+        let uncompressed_pubkey = public_key.to_encoded_point(false);
+        let public_key_bytes = &uncompressed_pubkey.as_bytes()[1..];
+
+        let hash = Keccak256::digest(public_key_bytes);
+        let mut address = [0u8; 20];
+        address.copy_from_slice(&hash[12..]);
+        address
+    }
+}
+
+#[cfg(test)]
+mod k256_test {
+    use super::*;
+    use crate::hex;
+
+    #[test]
+    fn sanity_ecrecover_call() {
+        let signature = hex!("650acf9d3f5f0a2c799776a1254355d5f4061762a237396a99a0e0e3fc2bcd6729514a0dacb2e623ac4abd157cb18163ff942280db4d5caad66ddf941ba12e0300");
+        let message = hex!("47173285a8d7341e5e972fc677286384f802f8ef42a5ec5f03bbfa254cb01fad");
+
+        let signer_address = k256::recover_signer_unchecked(&signature, &message)
+            .expect("Failed to recover signer address");
+        // println!("Recovered address: {:?}", hex::encode(signer_address));
+        assert_eq!(
+            signer_address,
+            hex::decode("c08b5542d177ac6686946920409741463a15dddb").unwrap().as_slice()
+        );
     }
 }
