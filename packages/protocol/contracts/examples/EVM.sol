a031b78e40f986dd2213384a29d8b844d7c45b84
Brecht Devos <Brechtp.Devos@gmail.com>
2024-12-12 20:14:37 +0100

    Fix EVM.sol for latest revm

--- packages/protocol/contracts/examples/EVM.sol
+++ packages/protocol/contracts/examples/EVM.sol
@@ -1,68 +1,71 @@
 // SPDX-License-Identifier: MIT
 
 pragma solidity >=0.8.12 <0.9.0;
-
 // EVM library
 library EVM {
-    // precompile addresses
     address constant xCallOptionsAddress = address(0x04D2);
 
     uint constant l1ChainId = 1;
-    uint constant version = 1;
+    uint16 constant version = 1;
 
     function xCallOnL1()
-        public 
-        view 
+        internal
+        view
     {
         xCallOptions(l1ChainId);
     }
 
-    function xCallOptions(uint chainID)
-        public 
-        view 
+    function xCallOnL1(bool sandbox)
+        internal
+        view
     {
-        xCallOptions(chainID, true);
+        xCallOptions(l1ChainId, sandbox);
+    }
+
+    function xCallOptions(uint chainID)
+        internal
+        view
+    {
+        xCallOptions(chainID, false);
     }
 
     function xCallOptions(uint chainID, bool sandbox)
-        public 
-        view 
+        internal
+        view
     {
-        xCallOptions(chainID, sandbox, address(0), address(0));
+        xCallOptions(chainID, sandbox, tx.origin, address(this));
     }
 
     function xCallOptions(uint chainID, bool sandbox, address txOrigin, address msgSender)
-        public 
-        view 
+        internal
+        view
     {
         xCallOptions(chainID, sandbox, txOrigin, msgSender, 0x0, "");
     }
 
     function xCallOptions(uint chainID, bool sandbox, bytes32 blockHash, bytes memory proof)
-        public 
-        view 
+        internal
+        view
     {
         xCallOptions(chainID, sandbox, address(0), address(0), blockHash, proof);
     }
 
     function xCallOptions(uint chainID, bool sandbox, address txOrigin, address msgSender, bytes32 blockHash, bytes memory proof)
-        public 
-        view 
+        internal
+        view
     {
-        // This precompile is not supported on L1
-        require(chainID != l1ChainId);
+        // require(chainID() != l1ChainId);
 
-        // Call the custom precompile
-        bytes memory input = abi.encodePacked(version, chainID, sandbox, txOrigin, msgSender, blockHash, proof);
-        (bool success, ) = xCallOptionsAddress.staticcall(input);
+        bytes memory input = abi.encodePacked(version, uint64(chainID), sandbox, txOrigin, msgSender, blockHash, proof);
+        (bool success, ) = xCallOptionsAddress.staticcall(input); //delegatecall
         require(success);
     }
 
-    function isOnL1() public view returns (bool) {
+    function isOnL1() internal view returns (bool) {
         return chainId() == l1ChainId;
     }
 
-    function chainId() public view returns (uint256) {
+    function chainId() internal view returns (uint256) {
         return block.chainid;
     }
 }
--- packages/protocol/scripts/xTransfer.s.sol
+++ packages/protocol/scripts/xTransfer.s.sol
@@ -13,22 +13,22 @@ contract XTransfer is Script {
     address ALICE = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Can stay as is - test values anyways
     address BOB = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8; //Can stay as is - test values anyways
     uint256 ALICE_PK = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;//Can stay as is - test values anyways
-    
+
     function setUp() public {}
 
     function run() public {
         address alice = vm.addr(ALICE_PK);
-        
+
         console.log("\n=== Before Transfer ===");
         //checkBalances(); -> EXPLORER
-        
+
         vm.startBroadcast(ALICE_PK);
 
         // Transfer 666 tokens to Bob on L2B (chainId: 167011)
-        xERC20(TOKEN_ADDRESS).xTransfer(167011, BOB, 666);
+        xERC20(TOKEN_ADDRESS).xTransfer(167010, BOB, 666);
 
         vm.stopBroadcast();
-        
+
         console.log("\n=== After Transfer ===");
         //checkBalances(); -> EXPLORER
     }
