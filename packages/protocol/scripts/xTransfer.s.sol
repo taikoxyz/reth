1372ac42dc1f5881477afe88063e0389f8ae55c6
Keszey Dániel <keszeyd@MacBook-Pro.local>
2024-12-13 10:58:22 +0100

    changed contract address

--- packages/protocol/scripts/xTransfer.s.sol
+++ packages/protocol/scripts/xTransfer.s.sol
@@ -8,7 +8,7 @@ import "../contracts/examples/xERC20.sol";
 
 contract XTransfer is Script {
     // The deployed contract address (will be the same on both chains due to deterministic deployment)
-    address constant TOKEN_ADDRESS = 0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512;
+    address constant TOKEN_ADDRESS = 0x5FbDB2315678afecb367f032d93F642f64180aa3;
 
     address ALICE = 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266; //Can stay as is - test values anyways
     address BOB = 0x70997970C51812dc3A010C7d01b50e0d17dc79C8; //Can stay as is - test values anyways
